c40715f1faabaf5a06096fb344dc6662057d10ad
Showket Ahmed <strblackhawk@gmail.com>
2026-01-09 23:58:48 +0600

    Connect Helper Modified for only default database connection

--- src/Helper/Connect.php
+++ src/Helper/Connect.php
@@ -26,16 +26,16 @@ class Connect
     public static function db(): void
     {
         // Get Database Configs
-        $configs = Config::get('database', default:[]);
-        // Start All Connections
-        if (!empty($configs)) {
-            foreach ($configs as $name => $config) {
-                try {
-                    Connection::add($config, $name);
-                } catch (\Throwable $th) {
-                    throw new HttpException(500, "'{$name}' Database Error: {$th->getMessage()}", $th->getCode());
-                }
-            }
+        $config = Config::get('database', 'default');
+        // Check 'default' Connection Exists
+        if (empty($config)) {
+            throw new \RuntimeException("Database [default] Connection Name Doesn't Exists!");
+        }
+        // Start Default Connections
+        try {
+            Connection::add($config);
+        } catch (\Throwable $e) {
+            throw new HttpException(500, "Database Connection Error: {$e->getMessage()}");
         }
         return;
     }
@@ -47,7 +47,7 @@ class Connect
     public static function timezone(): void
     {
         // Set Date Time
-        date_default_timezone_set(\do_hook('time.zone', 'Europe/London'));
+        date_default_timezone_set(\do_hook('option', 'time.zone', 'Europe/London'));
     }
 
     /**
