f214fb8e93826c6f04b58f3288e0c22686ddded1
Showket Ahmed <strblackhawk@gmail.com>
2026-01-10 00:26:46 +0600

    Date test argument fixed

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -3,9 +3,9 @@ namespace Laika\Tests;
 
 // Define APP_PATH for Laika Core
 if (!defined('APP_PATH')) {
-    define('APP_PATH', dirname(__DIR__) . '/');
+    define('APP_PATH', dirname(__DIR__));
 }
 
 // Load Composer autoloader
-require dirname(__DIR__) . '/vendor/autoload.php';
+require APP_PATH . '/vendor/autoload.php';
 
