a89671a8a76804537bc5e7b4fb1cfec1fd9c1e5f
Showket Ahmed <strblackhawk@gmail.com>
2026-01-10 23:29:41 +0600

    message.get modified for parameters

--- helpers/hooks/message.hook.php
+++ helpers/hooks/message.hook.php
@@ -15,8 +15,12 @@ use Laika\Session\Session;
 ####################################################################
 /*----------------------- MESSAGE FILTERS ------------------------*/
 ####################################################################
-// Set Notification Message
-add_hook('message.set', function(?string $message, bool $status): void {
+/**
+ * Set Notification Message
+ * @param string $message Message to Set
+ * @param bool $status Warning or Success. true for Success & false for Warning
+ */
+add_hook('message.set', function(string $message, bool $status): void {
     Session::set('message', ['info'=>$message,'status'=>$status]);
     return;
 });
