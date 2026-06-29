a2e1801919ce6943c7be1b73ddf08c288c4b2544
Showket Ahmed <strblackhawk@gmail.com>
2026-06-30 03:48:48 +0600

    Default Schemas Namespace bug fixed

--- src/Schema/ActivitySchema.php
+++ src/Schema/ActivitySchema.php
@@ -11,7 +11,7 @@
 declare(strict_types=1);
 
 // Namespace
-namespace Laika\Core\Migration;
+namespace Laika\Core\Schema;
 
 // Deny Direct Access
 defined('APP_PATH') || http_response_code(403) . die('403 Direct Access Denied!');
